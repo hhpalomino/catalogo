f4df11c80fe282a617bebbc3eb16120556f0acb9
Hector Palomino <Hector.palomino@eglo.cl>
2026-02-06 22:48:56 -0300

    fix: usar DATABASE_POOL_URL para evitar max clients

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,10 +2,13 @@ import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
+const datasourceUrl = process.env.DATABASE_POOL_URL || process.env.DATABASE_URL;
+
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
     log: ["error"],
+    datasourceUrl,
   });
 
 if (process.env.NODE_ENV !== "production")
